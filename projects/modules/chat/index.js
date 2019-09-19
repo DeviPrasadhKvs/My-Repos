4967e84da4a142d7b4678dafdbbf77a134e3fee1
Dsp.97 <deviprasadh.kvs@theotherfruit.com>
2019-09-19 20:14:27 +0530

    19/09

--- projects/modules/chat/index.js
+++ projects/modules/chat/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const data = require('./newFlowS.json')
+const data = require('./flow.json')
 const mapper = require('./textMapper.json')
 const messages = require('./messages.json')
 
@@ -8,76 +8,61 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.send("It works!");
 });
 
-app.get("/text/:data", function(req, res){
-    var re = req.params.data
-    if(re.includes('*')){
-        d = 
-        re.split('_')
-        var samp = {}
-        d.forEach(k=>{
-            var val = k.split('*')
-            samp[val[0]] = val[1]
-        })
-        // flow = re.split('_')
-        res.send(samp)
-    }else{
-        if(re.includes('&')){
-            d = data[re.replace('&','')]
-        
-            res.send(d)
-        }else{
-    
-        var state = mapper[re].split('@')
-        if(state.length>1){
-            res.send({[state[0]]:state[1].split(',')});
-        }else{
-            res.send({[re]:mapper[re]});
+app.get("/text/:data", function(req, res) {
+    var value = req.params.data
+    if (value.includes('*')) {
+        splitData = value.split('_')
+        var sampleData = {}
+        splitData.forEach(func => {
+                var val = func.split('*')
+                sampleData[val[0]] = val[1]
+            })
+            // flow = re.split('_')
+        res.send(sampleData);
+    } else {
+        if (value.includes('&')) {
+            splitData = data[value.replace('&', '')]
+            res.send(splitData)
+
+        } else {
+            var state = mapper[value].split('@')
+            if (state.length > 1) {
+                res.send({
+                    [state[0]]: state[1].split(',')
+                });
+            } else {
+                res.send({
+                    [value]: mapper[value]
+                });
+            }
         }
     }
-}
 
 });
 
-app.get("/newFlow/:data", function(req, res){
-    var re = req.params.data
-    if(re.includes('_')){
-        d = re.split('_')
-        var samp = {}
-        d.forEach(k=>{
-            var val = k.split('*')
-            samp[val[0]] = val[1]
+app.get("/newFlow/:data", function(req, res) {
+    var value = req.params.data
+    if (value.includes('_')) {
+        splitData = value.split('_')
+        var sampleData = {}
+        splitData.forEach(func => {
+            var val = func.split('*')
+            sampleData[val[0]] = val[1]
         })
-        res.send(samp)
-    }else{
-        var d = {
-            data:messages[re],
-            options:customFilter(data,re.trim())
+        res.send(sampleData)
+    } else {
+        var splitData = {
+            data: messages[value],
+            options: customFilter(data, value.trim())
         }
-        res.send(d)
+        res.send(splitData)
     }
 });
 
-function customFilter(object,re){
-    console.log(re);
-    
-    if(object.hasOwnProperty(re) && object[re] == 1)
-        return object;
-
-    for(var i=0; i<Object.keys(object).length; i++){
-        if(typeof object[Object.keys(object)[i]] == "object"){
-            var o = customFilter(object[Object.keys(object)[i]],re);
-            if(o != null)
-                return o;
-        }
-    }
-
-    return null;
-}
-
-app.listen(4000, (err, res)=>{
+app.listen(4000, (err, res) => {
     console.log('Connection Established');
 })
