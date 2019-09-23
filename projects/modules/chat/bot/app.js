b51ef32543d2eabc84414ac7cd758476874c9dd9
DeviPrasadh <deviprasadh.kvs@theotherfruit.com>
2019-09-24 00:51:48 +0530

    .

--- projects/modules/chat/bot/app.js
+++ projects/modules/chat/bot/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express();
-const ejs = require('ejs')
 let cors = require('cors')
 
 
@@ -10,17 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
 
 var chatController = require('./chatController')
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
 
     chatController(app)
-    res.render('index');
-})
+// })
 
 // initDB(() => {
 app.listen(4000, (err, res) => {
--- projects/modules/chat/bot/chatController.js
+++ projects/modules/chat/bot/chatController.js
@@ -1,11 +1,12 @@
+
 let flow = require('./flow.json')
 let messageLinker = require('./messageFlow.json')
 
 module.exports = (app) => {
 
-    app.get('/', (req, res) => {
-        res.status(200).send('Server is up and running')
-    });
+    // app.get('/', (req, res) => {
+    //     res.status(200).send('Server is up and running')
+    // });
 
     app.get('/chat/:data', (req, res) => {
         console.log(req.params);
@@ -14,7 +15,7 @@ module.exports = (app) => {
         if (value.includes('*')) {
             var data = value.split('*')
             var splitData = flow[data[0]].replace('{{data}}', data[1])
-            res.send(splitData)
+            res.send({status:2,data:splitData})
                 // res.send()
         } else {
             if (value === 'init') {
@@ -22,13 +23,13 @@ module.exports = (app) => {
                 flow['A00'].forEach(element => {
                     splitData.push(messageLinker[element])
                 });
-                res.send(splitData)
+                res.send({status:1,data:splitData})
             } else {
                 var splitData = []
                 flow[value].forEach(element => {
                     splitData.push(messageLinker[element])
                 });
-                res.send(splitData)
+                res.send({status:1,data:splitData})
             }
         }
     })
