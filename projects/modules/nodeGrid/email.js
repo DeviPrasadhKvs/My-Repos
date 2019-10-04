ecf81f81b21d9e5809c70fb5e1c7151ae29529a4
Dsp.97 <deviprasadh.kvs@theotherfruit.com>
2019-10-04 20:22:44 +0530

    04/10

--- projects/modules/nodeGrid/email.js
+++ projects/modules/nodeGrid/email.js
@@ -2,16 +2,13 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(process.env.SENDGRID_KEY);
 
-module.exports.send = function send(html,subject,to,from){
-
-	return new Promise((resolve,reject)=>{
-
-		sgMail.send({
-			to,
-			from,
-			subject,
-			html
-		}).then(resolve).catch(reject);
-				
-	});
+module.exports.send = function send(html, subject, to, from) {
+    return new Promise((resolve, reject) => {
+        sgMail.send({
+            to,
+            from,
+            subject,
+            html
+        }).then(resolve).catch(reject);
+    });
 };
